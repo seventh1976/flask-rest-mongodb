e6a5fecfad5a5a0eb1fee9ccaa05db6b08f1ec9d
seventh1976 <seventh1976@gmail.com>
2020-03-15 13:24:26 +0800

    Added update user

--- main.py
+++ main.py
@@ -48,7 +48,22 @@ def user(id):
 # Endpoint to update user
 @app.route('/update', methods=['PUT'])
 def update_user():
-    pass
+    _json = request.json
+    _id = _json['_id']
+    _name = _json['name']
+    _email = _json['email']
+    _password = _json['pwd']
+    # validate the received values
+    if _name and _email and _password and _id and request.method == 'PUT':
+        # hash the password
+        _hashed_password = generate_password_hash(_password)
+        # save edits
+        mongo.db.user.update_one({'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'name': _name, 'email': _email, 'pwd': _hashed_password}})
+        resp = jsonify('User updated successfully!')
+        resp.status_code = 200
+        return resp
+    else:
+        return not_found()
 
 
 # Endpoint to delete user
