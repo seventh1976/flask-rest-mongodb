0ccfb57d9494620cab9185eea09bf895ba9acd00
seventh1976 <seventh1976@gmail.com>
2020-03-15 12:39:12 +0800

    Pip8 indentation

--- main.py
+++ main.py
@@ -8,66 +8,68 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # Endpoint for creating new user
 @app.route('/add', methods=['POST'])
 def add_user():
-  _json = request.json
-  _name = _json['name']
-  _email = _json['email']
-  _password = _json['pwd']
-  # Validate the received values
-  if _name and _email and _password and request.method == 'POST':
-    # do not save password as a plain text
-    _hashed_password = generate_password_hash(_password)
-    # save details
-    id = mongo.db.user.insert({
-      'name': _name,
-      'email': _email,
-      'pwd': _hashed_password
-    })
-    resp = jsonify('User added successfully!')
-    resp.status_code = 200
-    return resp
-  else:
-    return not_found()
+    _json = request.json
+    _name = _json['name']
+    _email = _json['email']
+    _password = _json['pwd']
+    # Validate the received values
+    if _name and _email and _password and request.method == 'POST':
+        # do not save password as a plain text
+        _hashed_password = generate_password_hash(_password)
+        # save details
+        id = mongo.db.user.insert({
+            'name': _name,
+            'email': _email,
+            'pwd': _hashed_password
+        })
+        resp = jsonify('User added successfully!')
+        resp.status_code = 200
+        return resp
+    else:
+        return not_found()
 
 
 # Endpoint to list all user
 @app.route('/users')
 def users():
-  users = mongo.db.user.find()
-  resp = dumps(users)
-  return resp
+    users = mongo.db.user.find()
+    resp = dumps(users)
+    return resp
 
 
 # Endpoint to find a user
 @app.route('/user/<id>')
 def user(id):
-  pass
+    user = mongo.db.user.find_one({'_id': ObjectId(id)})
+    resp = dumps(user)
+    return resp
 
 
 # Endpoint to update user
 @app.route('/update', methods=['PUT'])
 def update_user():
-  pass
+    pass
 
 
 # Endpoint to delete user
 @app.route('/delete/<id>', methods=['DELETE'])
 def delete_user(id):
-  pass
+    pass
 
 
 # 404 method to handle not found error
 @app.errorhandler(404)
 def not_found(error=None):
-  message = {
-    'status': 404,
-    'message': 'Not Found: ' + request.url,
-  }
-  resp = jsonify(message)
-  resp.status_code = 404
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
 
-  return resp
+    return resp
 
 
 # Run application
 if __name__ == '__main__':
-  app.run()
+    app.run()
